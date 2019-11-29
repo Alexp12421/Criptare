16cf5abbc61899ae1d6b329ee3dbf1315a4ad75d
Student <student@debian.localdomain>
2019-11-29 13:38:48 +0200

    a

---
+++ alex.h
@@ -0,0 +1,8 @@
+#ifndef ALEX_INCLUDED
+#define ALEX_INCLUDED
+
+void sort_et_pret_cresc(int v[101],int n);
+void Valori_sub_max(int v[101],int n,int val);
+void Matrice_in_vector(int a[11][101],int v[1111],int val,int m,int n);
+
+#endif // ALEX_INCLUDED
---
+++ main.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+///this is how you include a file from the same project.
+///the h file contains all the functions you want to include
+#include "func.h"
+#include "alex.h"
+
+
+
+int main()
+{
+
+
+
+
+
+
+}
