d19aac68d6c7a6732966b6fadea1e24d0bffbc0c
alexp12421 <alexpalescy@gmail.com>
2019-11-30 16:32:45 +0200

    git commit -am

--- main.c
+++ main.c
@@ -3,6 +3,10 @@
 ///the h file contains all the functions you want to include
 #include "func.h"
 #include "alex.h"
+#include "iasmina.h"
+#include "diana.h"
+#include "david.h"
+#include "filip.h"
 
 
 
