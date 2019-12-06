a25400b32a8869038e607d6104f2730c1c9c4364
Student <student@debian.localdomain>
2019-12-06 12:31:29 +0200

    asd

--- david.c
+++ david.c
@@ -1,1 +1,20 @@
 #include <stdio.h>
+#include <string.h>
+void stele1(char s[])
+{
+  int i,n=strlen(s);
+  for (i=0;i<n;i++)
+  {
+  if (s[i]==' ') s[i]='*';
+    else s[i];
+  }
+}
+void stele2(char s[])
+{
+  int i,n=strlen(s);
+  for (i=0;i<n;i++)
+  {
+  if (s[i]=='*') s[i]=' ';
+    else s[i];
+  }
+}
--- david.h
+++ david.h
@@ -1,6 +1,7 @@
 #ifndef DAVID_H_INCLUDED
 #define DAVID_H_INCLUDED
-
+void stele1(char s[]);
+void stele2(char s[]);
 
 
 #endif // DAVID_H_INCLUDED
